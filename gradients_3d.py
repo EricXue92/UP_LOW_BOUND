53a6839504663878d8aefb0fbdc7c94c4c5d7c84
EricXue92 <107911428+EricXue92@users.noreply.github.com>
2022-07-17 15:41:15 +0800

    minor improved

--- gradients_3d.py
+++ gradients_3d.py
@@ -5,10 +5,11 @@ import pandas as pd
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import MaxNLocator
+sns.set_theme(style="darkgrid")
+
 
 # Plot the ditribution 
 def plot_pred_distribution(file_name = 'no_pcgrad_pred.csv', x = 'Width'):
-
 	df = pd.read_csv(file_name)
 	sns.displot(data=df, x = x, kde=True)
 	sns.set_style("ticks")
@@ -30,9 +31,9 @@ def plot_val_loss_3D(file_name = 'history_no_pcgrad_history.pkl'):
 	#surf = ax.plot_trisurf(width-np.mean(width), coverage-np.mean(coverage), val_loss, cmap=cm.jet, linewidth=0)
 	surf = ax.plot_trisurf(width, coverage, val_loss, cmap=cm.jet, linewidth=0)
 	fig.colorbar(surf)
-	ax.set_title('val_Loss')
 	ax.set_xlabel('val_width')
 	ax.set_ylabel('val_coverage')
+	ax.set_title('val_Loss')
 	ax.xaxis.set_major_locator(MaxNLocator(10))
 	ax.yaxis.set_major_locator(MaxNLocator(10))
 	ax.zaxis.set_major_locator(MaxNLocator(10))
@@ -41,7 +42,6 @@ def plot_val_loss_3D(file_name = 'history_no_pcgrad_history.pkl'):
 	plt.show()
 
 # To show the same y_slim as history_no_pcgrad_histor
-
 def plot_pcgrad_history_with_same_yslim():
 	dict_data = pd.read_pickle('history_pcgrad_history.pkl')
 	df = pd.DataFrame(dict_data)
@@ -51,22 +51,38 @@ def plot_pcgrad_history_with_same_yslim():
 	plt.xlabel("Epochs")
 	ax = sns.lineplot(data=df[ ['coverage', 'mpiw', 'val_coverage', 'val_mpiw', 'val_loss']])
 	ax.set(ylim=(0, 1.4))
+	plt.show()
 	plt.savefig(f'history_pcgrad_ylim_png', dpi = 600)
+
+def plot_Coverage_rate_Width_rate(file_name = 'no_pcgrad_pred.csv'):
+	df = pd.read_csv(file_name)
+	x = np.array(range(0,len(df),1))
+	y = df['Coverage_rate/Width']
+	plt.scatter(x, y)
+	plt.savefig(f'{file_name}_coverage_rate_width.png')
 	plt.show()
 
+
 def main():
-	#Plot prediction distribution
-	plot_pred_distribution()
-	plot_pred_distribution(x = 'Coverage_rate')
-	plot_pred_distribution(file_name = 'pcgrad_pred.csv')
-	plot_pred_distribution(file_name = 'pcgrad_pred.csv', x ='Coverage_rate')
+	# #Plot prediction distribution
 
-	#Plot the loss_val, val_mpiw, val_coverage
-	plot_val_loss_3D()
-	plot_val_loss_3D('history_pcgrad_history.pkl')
+	# plot_pred_distribution()
+	# plot_pred_distribution(x = 'Coverage_rate')
+	# plot_pred_distribution(file_name = 'pcgrad_pred.csv')
+	# plot_pred_distribution(file_name = 'pcgrad_pred.csv', x ='Coverage_rate')
 
-	#Plot the same y_slim as history_no_pcgrad_history
-	plot_pcgrad_history_with_same_yslim()
+	# #Plot the loss_val, val_mpiw, val_coverage
+
+	# plot_val_loss_3D()
+	# plot_val_loss_3D('history_pcgrad_history.pkl')
+
+	# #Plot the same y_slim as history_no_pcgrad_history
+
+	# plot_pcgrad_history_with_same_yslim()
+
+	#plot Coverage_rate/Width
+	plot_Coverage_rate_Width_rate()
+	plot_Coverage_rate_Width_rate('pcgrad_pred.csv')
 
 
 if __name__ == "__main__":

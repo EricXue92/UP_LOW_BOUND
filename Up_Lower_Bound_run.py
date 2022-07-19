a384f2f4cb2fbd522f33595e3ef14c9d346ccf38
EricXue92 <107911428+EricXue92@users.noreply.github.com>
2022-07-19 16:07:55 +0800

    minor modification

--- Up_Lower_Bound_run.py
+++ Up_Lower_Bound_run.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import csv 
 
 import pickle
 import gc 
@@ -18,12 +19,9 @@ from Up_Lower_Bound import UpperLowerBound
 from tensorflow.keras import callbacks
 
 
-tf.random.set_seed(2)
+# tf.random.set_seed(2)
 
 class Run:
-
-   No_PCGrad = UpperLowerBound()
-   PCGrad = UpperLowerBound()
 
    epochs = 5
    batch_size = 256
@@ -36,10 +34,14 @@ class Run:
       restore_best_weights=True
    )
 
-   def __init__(self):
+   def __init__(self, No_PCGrad, PCGrad):
 
-      self.No_PCGrad_model = Run.No_PCGrad.model 
-      self.PCGrad_model = Run.PCGrad.model
+      self.No_PCGrad = No_PCGrad
+      self.PCGrad = PCGrad
+
+      self.No_PCGrad_model = No_PCGrad.model 
+      self.PCGrad_model = PCGrad.model
+
       self.result = []
       self.opt = optimizers.Adam()
 
@@ -59,34 +61,36 @@ class Run:
       loss = [self.No_PCGrad_model.selective_up, self.No_PCGrad_model.selective_low, self.No_PCGrad_model.up_penalty, self.No_PCGrad_model.low_penalty, self.No_PCGrad_model.coverage_penalty],
       metrics = [self.No_PCGrad_model.coverage, self.No_PCGrad_model.mpiw, self.No_PCGrad_model.coverage_width_rate])
 
-      history_no_pcgrad = self.No_PCGrad_model.fit(Run.No_PCGrad.X_train, Run.No_PCGrad.y_train, 
-      validation_data = (Run.No_PCGrad.X_test, [Run.No_PCGrad.y_test[:,0], Run.No_PCGrad.y_test[:,1], Run.No_PCGrad.y_test[:,-1]]),
+      history_no_pcgrad = self.No_PCGrad_model.fit(self.No_PCGrad.X_train, self.No_PCGrad.y_train, 
+      validation_data = (self.No_PCGrad.X_test, [self.No_PCGrad.y_test[:,0], self.No_PCGrad.y_test[:,1], self.No_PCGrad.y_test[:,-1]]),
       batch_size=self.batch_size, 
       epochs= self.epochs,
       #callbacks=[Run.early_stopping], 
       verbose=0)
 
       # Save the training history 
-      with open('history_no_pcgrad_history.pkl', 'wb') as handle:
-         pickle.dump(history_no_pcgrad.history, handle, protocol=pickle.HIGHEST_PROTOCOL)
+      # with open(f'{self.No_PCGrad.dataset_name}_no_pcgrad_history.pkl', 'wb+') as handle:
+      #    pickle.dump(history_no_pcgrad.history, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-      
-      #self.plot_training('history_no_pcgrad_history.pkl')
+      # self.plot_training(f'{self.No_PCGrad.dataset_name}_no_pcgrad_history.pkl')
 
-
-      no_pcgrad_pred = self.No_PCGrad_model.predict(Run.No_PCGrad.X_test)
+      no_pcgrad_pred = self.No_PCGrad_model.predict(self.No_PCGrad.X_test)
 
       df = pd.DataFrame(no_pcgrad_pred, columns = ['Lowerbound', 'Upbound', 'Coverage_rate'])
-      df['y_true'] = Run.No_PCGrad.y_test[:,0]
+
+      df['y_true'] = self.No_PCGrad.y_test[:,0]
       df['Width'] = (df['Upbound']-df['Lowerbound'])
-      df['NMPIW'] = (df['Upbound']-df['Lowerbound'])/ Run.No_PCGrad.range
+      df['NMPIW'] = (df['Upbound']-df['Lowerbound'])/ self.No_PCGrad.range
       df['Coverage_rate/Width'] = df['Coverage_rate']/df['Width']
+
+      df = df[['Lowerbound','y_true','Upbound','Coverage_rate','Width','NMPIW','Coverage_rate/Width']]
       #df['Flag']= np.where((df['Upbound'] >= df['y_true']) & (df['Lowerbound'] <= df['y_true']), 1, 0)  
 
+      # Save all predicted value
+      # with open(f'{self.No_PCGrad.dataset_name}_no_pcgrad_pred.csv', 'a') as f:
+      #    df.to_csv(f, header = True )
+
       self.result.append({'Coverage_rate':np.mean(df['Coverage_rate']), 'NMPIW':np.mean(df['NMPIW']),'Coverage_rate/Width':np.mean(df['Coverage_rate/Width'])})
-      
-      df.to_csv('no_pcgrad_pred.csv')
-   
 
    def run_pcgrad(self):
       self.PCGrad_model.init_arguments(method = 'PCGrad')
@@ -95,50 +99,56 @@ class Run:
       loss = [self.PCGrad_model.selective_up, self.PCGrad_model.selective_low, self.PCGrad_model.up_penalty, self.PCGrad_model.low_penalty, self.PCGrad_model.coverage_penalty],
       metrics = [self.PCGrad_model.coverage, self.PCGrad_model.mpiw, self.PCGrad_model.coverage_width_rate])
 
-      history_pcgrad = self.PCGrad_model.fit(Run.PCGrad.X_train, Run.PCGrad.y_train, 
-      validation_data = (Run.PCGrad.X_test, [Run.PCGrad.y_test[:,0], Run.PCGrad.y_test[:,1], Run.PCGrad.y_test[:,-1]]),
+      history_pcgrad = self.PCGrad_model.fit(self.PCGrad.X_train, self.PCGrad.y_train, 
+      validation_data = (self.PCGrad.X_test, [self.PCGrad.y_test[:,0], self.PCGrad.y_test[:,1], self.PCGrad.y_test[:,-1]]),
       batch_size=self.batch_size, 
       epochs= self.epochs,
       #callbacks=[Run.early_stopping], 
       verbose=0)
 
-      with open('history_pcgrad_history.pkl', 'wb') as handle:
-         pickle.dump(history_pcgrad.history, handle, protocol=pickle.HIGHEST_PROTOCOL)     
+      # Save the training history 
+      # with open(f'{self.PCGrad.dataset_name}_pcgrad_history.pkl', 'wb+') as handle:
+      #    pickle.dump(history_pcgrad.history, handle, protocol=pickle.HIGHEST_PROTOCOL)     
       #model.save_weights("checkpoints/{}".format(self.filename))
 
-      #self.plot_training('history_pcgrad_history.pkl')
+      #self.plot_training(f'{self.PCGrad.dataset_name}_pcgrad_history.pkl')
 
-      pcgrad_pred = self.PCGrad_model.predict(Run.PCGrad.X_test)
+      pcgrad_pred = self.PCGrad_model.predict(self.PCGrad.X_test)
 
       df = pd.DataFrame(pcgrad_pred, columns = ['Lowerbound', 'Upbound', 'Coverage_rate'])
-      df['y_true'] = Run.PCGrad.y_test[:,0]
+
+      df['y_true'] = self.PCGrad.y_test[:,0]
       df['Width'] = (df['Upbound']-df['Lowerbound'])
-      df['NMPIW'] = (df['Upbound']-df['Lowerbound'])/ Run.No_PCGrad.range
+      df['NMPIW'] = (df['Upbound']-df['Lowerbound'])/ self.No_PCGrad.range
       df['Coverage_rate/Width'] = df['Coverage_rate']/df['Width']
+
+      df = df[['Lowerbound','y_true','Upbound','Coverage_rate','Width','NMPIW','Coverage_rate/Width']]
       #df['Flag']= np.where((df['Upbound'] >= df['y_true']) & (df['Lowerbound'] <= df['y_true']), 1, 0) 
 
-      self.result.append({'Coverage_rate':np.mean(df['Coverage_rate']), 'NMPIW':np.mean(df['NMPIW']),'Coverage_rate/Width':np.mean(df['Coverage_rate/Width']) })
+      # Save all predicted value 
+      # with open(f'{self.PCGrad.dataset_name}_pcgrad_pred.csv', 'a') as f:
+      #    df.to_csv(f, header=True)
 
-      #Save predicted values 
-      df.to_csv('pcgrad_pred.csv')
+
+      self.result.append({'Coverage_rate':np.mean(df['Coverage_rate']), 'NMPIW':np.mean(df['NMPIW']),'Coverage_rate/Width':np.mean(df['Coverage_rate/Width']) })
 
    def print_comparison(self):
       res = pd.DataFrame(self.result, index = ['No_PCGrad', 'PCGrad'])
       print(res)
-      #return res 
+      return res
 
-   def plot_training(self, filename):
-      dict_data = pd.read_pickle(filename)  
-      df = pd.DataFrame(dict_data)
-      title = '-'.join(filename.split('_')[:-1])
-      fig = plt.figure(figsize=(10,6))
-      sns.set_style("ticks")
-      plt.title(title)
-      plt.xlabel("Epochs")
-      sns.lineplot(data=df[ ['coverage', 'mpiw','val_coverage', 'val_mpiw','val_loss']])
-      plt.savefig(f'{title}.png', dpi = 600)
-      plt.clf()
-      # plt.show()
+   # def plot_training(self, filename):
+   #    dict_data = pd.read_pickle(filename)  
+   #    df = pd.DataFrame(dict_data)
+   #    title = f'{filename}'
+   #    fig = plt.figure(figsize=(10,6))
+   #    sns.set_style("ticks")
+   #    plt.title(title)
+   #    plt.xlabel("Epochs")
+   #    sns.lineplot(data=df[ ['coverage', 'mpiw','val_coverage', 'val_mpiw','val_loss']])
+   #    plt.savefig(f'{title}.png', dpi = 600)
+   #    plt.clf()
+   #    plt.show()
      
 
 def main():
@@ -155,31 +165,24 @@ def main():
    'moisture content of raw material','steam pressure','main stem temperature','reheat steam temperature']
 
 
-   for dataset_name, target in zip(dataset_names, targets):
-      UpperLowerBound.dataset_name, UpperLowerBound.target  = dataset_name, target 
-      for i in range(2):
-         obj = Run()
+   for index, (dataset_name, target) in enumerate(zip(dataset_names, targets)):
+      temp = []
+      for i in range(5):
+         No_PCGrad = UpperLowerBound(dataset_name, target)
+         PCGrad = UpperLowerBound(dataset_name, target)
+         
+         obj = Run(No_PCGrad, PCGrad)
          obj.run_no_pcgrad()
          obj.run_pcgrad()
-
-
-
-   # for i in range(2):
-   #    temp = []
-   #    for dataset_name, target in zip(dataset_names, targets):
-   #       UpperLowerBound.dataset_name, UpperLowerBound.target  = dataset_name, target 
-   #       obj = Run()
-   #       obj.run_no_pcgrad()
-   #       obj.run_pcgrad()
-   #       res = obj.print_comparison()
-   #       temp.append(res)
-   #    output = pd.concat(temp, keys=["1", "2", "3","4","5","6","7","8","9","10"])
-   #    output.to_csv(f'output_{i}.csv')
-
-
-      
+         res = obj.print_comparison()
+         temp.append(res)
+      output = pd.concat(temp)
+      output.to_csv(f'output_{index+1}.csv')
+ 
 if __name__ == "__main__":
+   # set the epochs 
    Run.epochs = 5
+   # set the batch_size 
    # Run.batch_size = 1000
    main()
 
